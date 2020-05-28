68268dc46cb6efb8cada4cf00ede82596d6bb96d
Hamed Zarei <hamedzarei.7232@gmail.com>
2020-05-28 10:25:07 +0430

    init

---
+++ readme.md
@@ -0,0 +1,8 @@
+# Sample Firefox Extension
+
+
+navigate to about:debugging#/runtime/this-firefox
+
+Click on Load Temporary Add-on, select one file from extension directory and click reload on that extension
+
+[Reference](https://developer.mozilla.org/en-US/docs/Mozilla/Add-ons/WebExtensions/Your_first_WebExtension)
---
+++ test_extension.js
@@ -0,0 +1,28 @@
+document.body.style.border = "5px solid red";
+
+function setCookie(cname, cvalue, exdays) {
+  var d = new Date();
+  d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+  var expires = "expires="+d.toUTCString();
+  document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+
+function getCookie(cname) {
+  var name = cname + "=";
+  var ca = document.cookie.split(';');
+  for(var i = 0; i < ca.length; i++) {
+    var c = ca[i];
+    while (c.charAt(0) == ' ') {
+      c = c.substring(1);
+    }
+    if (c.indexOf(name) == 0) {
+      return c.substring(name.length, c.length);
+    }
+  }
+  return "";
+}
+
+console.log("helloooooo");
+console.log(document.cookie);
+setCookie("test", "value", 1);
+console.log(getCookie("test"));
